cd637cbfcccef57ef0d9d3aba93017f41cf59557
alyvofficial <alyvofficial@gmail.com>
2023-09-14 16:25:01 +0400

    Added Facebook domain veri.

--- Assets/JS/pictureAnimation.js
+++
@@ -1,6 +0,0 @@
-function imgChanger () {
-    document.getElementById("myImg").src = "./Assets/Images/myImage(2).png"
-}
-function imgReset () {
-    document.getElementById("myImg").src = "./Assets/Images/myImage.png"
-} 
